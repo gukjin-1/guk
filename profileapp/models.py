66cf4e31f5889840448b11b9e9f7a7a783cde08c
dropl <https://github.com/gukjin-1/guk.git>
2021-07-26 11:27:30 +0900

    7/26 6class commit 1

--- profileapp/models.py
+++ profileapp/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+#CASCADE 관련된거 삭제
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='profile')
+
+    image = models.ImageField(upload_to='profile/', null=True) ##경로지정옵션
+    nickname = models.CharField(max_length=30, unique=True, null=True)
+    message = models.CharField(max_length=200, null=True)
